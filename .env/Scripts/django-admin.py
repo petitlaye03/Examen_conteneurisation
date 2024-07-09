317890da4549140d03390ab2af5b24f4bb6bbf83
petitlaye03 <ndourabdoulaye1803@gmail.com>
2024-07-09 16:07:00 +0000

    ABMS

---
+++ .env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Cours\L2\2nd semestre\Virtualisation\Examen_conteneurisation\.env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
