32925edd7f86cd498a9a15e107d1d3b84def51c7
petitlaye03 <124119995+petitlaye03@users.noreply.github.com>
2024-07-11 12:33:19 +0000

    Update forms.py

--- hospital/forms.py
+++ hospital/forms.py
@@ -4,7 +4,7 @@ from . import models
 
 
 
-#for admin signup
+
 class AdminSigupForm(forms.ModelForm):
     class Meta:
         model=User
@@ -14,7 +14,7 @@ class AdminSigupForm(forms.ModelForm):
         }
 
 
-#for student related form
+
 class DoctorUserForm(forms.ModelForm):
     class Meta:
         model=User
@@ -29,7 +29,6 @@ class DoctorForm(forms.ModelForm):
 
 
 
-#for teacher related form
 class PatientUserForm(forms.ModelForm):
     class Meta:
         model=User
@@ -38,9 +37,6 @@ class PatientUserForm(forms.ModelForm):
         'password': forms.PasswordInput()
         }
 class PatientForm(forms.ModelForm):
-    #this is the extrafield for linking patient and their assigend doctor
-    #this will show dropdown __str__ method doctor model is shown on html so override it
-    #to_field_name this will fetch corresponding value  user_id present in Doctor model and return it
     assignedDoctorId=forms.ModelChoiceField(queryset=models.Doctor.objects.all().filter(status=True),empty_label="Nom et Département", to_field_name="user_id")
     class Meta:
         model=models.Patient
@@ -63,14 +59,8 @@ class PatientAppointmentForm(forms.ModelForm):
         fields=['description','status']
 
 
-#for contact us page
+
 class ContactusForm(forms.Form):
     Name = forms.CharField(max_length=30)
     Email = forms.EmailField()
     Message = forms.CharField(max_length=500,widget=forms.Textarea(attrs={'rows': 3, 'cols': 30}))
-
-
-
-#Developed By : sumit kumar
-#facebook : fb.com/sumit.luv
-#Youtube :youtube.com/lazycoders
